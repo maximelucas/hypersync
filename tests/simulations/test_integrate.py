b1c488ff322ece608a2d07553812f82bbb493433
Maxime Lucas <ml.maximelucas@gmail.com>
2023-10-19 12:59:20 +0200

    tests: initial

---
+++ tests/simulations/test_integrate.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pytest
+
+import hypersync as hs
+
+def test_rhs_ring_nb():
+
+    omega = 0
+    k1 = 1
+    k2 = 1
+    r = 2
+
+    psi = np.array([0, np.pi, 0, np.pi])
+    psi_out = hs.rhs_ring_nb(0, psi, omega, k1, k2, r)

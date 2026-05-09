8f2f01942a4abe17d9be9cfb51096c6fb9c37cdf
Maxime Lucas <maxime.lucas.work@gmail.com>
2026-05-09 20:22:54 +0200

    fix: replaced old pkg_resources

--- hypersync/__init__.py
+++ hypersync/__init__.py
@@ -1,8 +1,8 @@
-import pkg_resources
+from importlib.metadata import version
 
 from . import analysis, drawing, simulation
 from .analysis import *
 from .drawing import *
 from .simulation import *
 
-__version__ = pkg_resources.require("hypersync")[0].version
+__version__ = version("hypersync")
